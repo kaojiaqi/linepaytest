154b77318b5182741a45513dbfc9e301d80d1bbb
kaojiaqi <st112343@gmail.com>
2017-11-22 14:15:16 +0800

    1. Install axios

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const axios = require('axios')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
 app.use('/',express.static(__dirname + "/public/views"))
 
+app.get('/test',(req,res) => {
+    res.send("test")
+})
 
-app.listen(process.env.port || 3000);
+app.listen(process.env.PORT || 3000);
