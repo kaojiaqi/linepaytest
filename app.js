d6a8b6724ed72997e2fa45d8f8d459510c441a19
kaojiaqi <st112343@gmail.com>
2017-11-22 14:03:09 +0800

    1. 修改 folder 名稱 2. index.html 新增 button , setupSDK

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
-app.use('/',express.static(__dirname + "/public/html"))
+app.use('/',express.static(__dirname + "/public/views"))
 
 
 app.listen(process.env.port || 3000);
